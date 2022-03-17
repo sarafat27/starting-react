31993c9e09183f2b1c0fb46e3e88a9339cd72dc6
sarafat27 <sarafat2769@gmail.com>
2022-03-18 01:59:00 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
+}
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([])
+  // useEffect(()=>{},[])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div style={{ border: '3px solid black', backgroundColor: 'lightblue', margin: '20px' }
+    }>
+      <h3>Name: {props.name}</h3>
+      <p>email: {props.email}</p>
+    </div >
+  )
 }
 
 function Counter() {
